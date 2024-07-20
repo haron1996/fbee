458786ad9aadae772dd984c7c0f5295f9432b85a
Kwandapchumba <kwandapchumba@gmail.com>
2024-07-20 09:46:58 +0300

    updated

--- main.go
+++ main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"time"
-
 	"github.com/haron1996/fb/funcs"
 )
 
 func main() {
 	funcs.LoginToFacebook()
-	time.Sleep(5 * time.Second)
 	funcs.PostToMarketplace()
 	funcs.ListInMorePlaces()
 }
